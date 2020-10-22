af15fb2bb0dd97a520e64978224ebb4ad1020749
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-10-22 11:35:34 +0530

    after debug

--- MyFirstProject.js
+++ MyFirstProject.js
@@ -1,4 +1,16 @@
 exports.handler = async (event) => {
-    
-    return {"message": "Successfully executed"};
+    debugger;
+    console.log("Test1");
+    console.log("Test2");
+    console.log("Test3");
+
+    console.log("Test4");
+    console.log("Test5");
+
+
+    console.log("Test6");
+    console.log("Test7");
+    console.log("Test8");
+
+    return { "message": "Successfully executed" };
 };
