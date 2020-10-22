9d4de52575f202afe0b5728ea73eadcb8d61de08
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-10-22 18:56:34 +0530

    dsjak

--- MyFirstProject.js
+++ MyFirstProject.js
@@ -1,16 +1,4 @@
 exports.handler = async (event) => {
-    debugger;
-    console.log("Test1");
-    console.log("Test2");
-    console.log("Test3");
-
-    console.log("Test4");
-    console.log("Test5");
-
-
-    console.log("Test6");
-    console.log("Test7");
-    console.log("Test8");
-
-    return { "message": "Successfully executed" };
+    
+    return {"message": "Successfully executed"};
 };
