ae3c3552874cebdef24aa6d26c8f376336c73fd0
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2021-05-03 18:22:08 +0530

    test

--- MyFirstProject.js
+++ MyFirstProject.js
@@ -1,21 +1,4 @@
-const AWS = require('aws-sdk');
-const cognito_idp = new AWS.CognitoIdentityServiceProvider();
-
 exports.handler = async (event) => {
-    debugger;
-    try {
-        let data = await cognito_idp.listUsers({
-            UserPoolId: "us-east-1_jfB99rZCt",
-            Limit: 10
-        }).promise();
-
-        console.log(data);
-
-    } catch (err) {
-        // error handling goes here
-        console.log(err);
-
-    };
-
-    return { "message": "Successfully executed" };
+    
+    return {"message": "Successfully executed"};
 };
